0e6a03837c870430e0b78b14614e04861845abb4
Tomasz Krasieńko <94125393+TomaszKrasienko@users.noreply.github.com>
2024-02-10 15:03:18 +0100

    Added configuration of hosted services

--- Rest.Client.Builder/lib/rest.client.builder/Configuration/Extensions.cs
+++ Rest.Client.Builder/lib/rest.client.builder/Configuration/Extensions.cs
@@ -1,6 +1,15 @@
+using Microsoft.Extensions.DependencyInjection;
+using rest.client.builder.HostedServices;
+
 namespace rest.client.builder.Configuration;
 
-public class Extensions
+public static class Extensions
 {
-    
+    public static IServiceCollection AddRestClientBuilder(this IServiceCollection services)
+        => services
+            .AddHostedServices();
+
+    private static IServiceCollection AddHostedServices(this IServiceCollection services)
+        => services
+            .AddHostedService<ExecutionService>();
 }
